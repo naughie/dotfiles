9de935cac3a61f358f3075e7b9a5a1452122b72c
Naughie <zomacchi@gmail.com>
2017-07-19 11:26:44 +0900

    Add picture displaying

--- setup.sh
+++ setup.sh
@@ -25,6 +25,7 @@ rm $HOME/.pryrc 2>/dev/null
 rm ${NVIM_CONFIG}/init.vim 2>/dev/null
 rm ${NVIM_CONFIG}/dein.toml 2>/dev/null
 rm ${NVIM_CONFIG}/lazy_dein.toml 2>/dev/null
+rm $HOME/.ryuou.jpg 2>/dev/null
 rm -r ${NVIM_CONFIG}/ftdetect 2>/dev/null
 rm -r ${NVIM_CONFIG}/colors 2>/dev/null
 mkdir ${NVIM_CONFIG}/ftdetect 2>/dev/null
@@ -42,21 +43,23 @@ ln -s ${CONFIG_DIR}/ftdetect ${NVIM_CONFIG}/ftdetect
 ln -s ${CONFIG_DIR}/colors ${NVIM_CONFIG}/colors
 ln -s ${CONFIG_DIR}/.latexmkrc $HOME/.latexmkrc
 ln -s ${CONFIG_DIR}/.pryrc $HOME/.pryrc
+ln -s ${CONFIG_DIR}/images/ryuou.jpg $HOME/.ryuou.jpg
 
 echo '今回作成したシンボリックリンクはこちら！'
-echo  "$HOME/.zshrc -> ${CONFIG_DIR}/.zshrc"
-echo  "$HOME/.zshenv -> ${CONFIG_DIR}/.zshenv"
-echo  "$HOME/.zprofile -> ${CONFIG_DIR}/.zprofile"
-echo  "$HOME/.vimrc -> ${CONFIG_DIR}/.vimrc"
-echo  "$HOME/.dein.toml -> ${CONFIG_DIR}/dein.toml"
-echo  "$HOME/.lazy_dein.toml -> ${CONFIG_DIR}/lazy_dein.toml"
-echo  "$HOME/.latexmkrc -> ${CONFIG_DIR}/.latexmkrc"
-echo  "$HOME/.pryrc -> ${CONFIG_DIR}/.pryrc"
-echo  "${NVIM_CONFIG}/init.vim -> ${CONFIG_DIR}/.vimrc"
-echo  "${NVIM_CONFIG}/dein.toml -> ${CONFIG_DIR}/dein.toml"
-echo  "${NVIM_CONFIG}/lazy_dein.toml -> ${CONFIG_DIR}/lazy_dein.toml"
-echo  "${NVIM_CONFIG}/ftdetect -> ${CONFIG_DIR}/ftdetect"
-echo  "${NVIM_CONFIG}/colors -> ${CONFIG_DIR}/colors"
+echo "$HOME/.zshrc -> ${CONFIG_DIR}/.zshrc"
+echo "$HOME/.zshenv -> ${CONFIG_DIR}/.zshenv"
+echo "$HOME/.zprofile -> ${CONFIG_DIR}/.zprofile"
+echo "$HOME/.vimrc -> ${CONFIG_DIR}/.vimrc"
+echo "$HOME/.dein.toml -> ${CONFIG_DIR}/dein.toml"
+echo "$HOME/.lazy_dein.toml -> ${CONFIG_DIR}/lazy_dein.toml"
+echo "$HOME/.latexmkrc -> ${CONFIG_DIR}/.latexmkrc"
+echo "$HOME/.pryrc -> ${CONFIG_DIR}/.pryrc"
+echo "$HOME/.ryuou.jpg -> ${CONFIG_DIR}/images/ryuou.jpg"
+echo "${NVIM_CONFIG}/init.vim -> ${CONFIG_DIR}/.vimrc"
+echo "${NVIM_CONFIG}/dein.toml -> ${CONFIG_DIR}/dein.toml"
+echo "${NVIM_CONFIG}/lazy_dein.toml -> ${CONFIG_DIR}/lazy_dein.toml"
+echo "${NVIM_CONFIG}/ftdetect -> ${CONFIG_DIR}/ftdetect"
+echo "${NVIM_CONFIG}/colors -> ${CONFIG_DIR}/colors"
 
 if [ -f '/etc/centos-release' ] ; then
   if [ ${has_zsh} -ne 0 ] ; then
@@ -83,4 +86,12 @@ if [ -f '/etc/centos-release' ] ; then
     export EDITOR=nvim
     echo '初期設定をするため，neovimを起動するよ！'
   fi
+fi
+
+which imgcat 1>/dev/null 2>&1
+if [ $? -ne 0 ] ; then
+  curl 'https://raw.githubusercontent.com/gnachman/iTerm2/master/tests/imgcat' -o imgcat
+  chmod +x imgcat
+  mv imgcat /usr/local/bin
+  echo 'imgcatをdownloadしたよ！'
 fi
