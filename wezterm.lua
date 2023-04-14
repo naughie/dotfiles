44221534843baee7855cfaee748711f0bc5c1207
Masato Nakata <masaton@naughie.com>
2023-04-14 11:33:21 +0900

    Update wezterm config

--- wezterm.lua
+++ wezterm.lua
@@ -1,16 +1,15 @@
 local wezterm = require 'wezterm';
-return {
-    font = wezterm.font("Source Han Code JP L"),
-    font_size = 15,
-    colors = {
-        foreground = "#eeeeee",
-        background = "#282828",
-        selection_bg = "#293b44",
-        selection_fg = "#ffffff",
 
-        ansi = {"#282828", "#f43753", "#c9d05c", "#ffc24b", "#b3deef", "#d3b987", "#73cef4", "#eeeeee"},
-        brights = {"#1d1d1d", "#f43753", "#c9d05c", "#ffc24b", "#b3deef", "#d3b987", "#73cef4", "#ffffff"},
-    },
+local config = {
+    font = wezterm.font("Source Han Code JP L"),
+    font_size = 12,
+
+    -- color_scheme = 'everforest light',
+    color_scheme = 'everforest dark',
+
+    window_background_opacity = 1.0,
+
+    use_fancy_tab_bar = false,
 
     scrollback_lines = 4096,
 
@@ -33,4 +32,94 @@ return {
     exit_behavior = "Close",
 
     tab_bar_at_bottom = true,
+
+    warn_about_missing_glyphs = false,
+
+    show_update_window = false,
 }
+
+config.color_schemes = {
+    ['tender'] = {
+        foreground = "#eeeeee",
+        background = "#282828",
+        selection_fg = "#ffffff",
+        selection_bg = "#293b44",
+
+        ansi = {"#282828", "#f43753", "#c9d05c", "#ffc24b", "#b3deef", "#d3b987", "#73cef4", "#eeeeee"},
+        brights = {"#1d1d1d", "#f43753", "#c9d05c", "#ffc24b", "#b3deef", "#d3b987", "#73cef4", "#ffffff"},
+    },
+
+    ['everforest dark'] = {
+        foreground = "#d3c6aa",
+        background = "#333c43",
+        selection_bg = "#5c3f4f",
+
+        ansi = {"#333c43", "#e67e80", "#a7c080", "#dbbc7f", "#7fbbb3", "#d699b6", "#83c092", "#d3b987"},
+        brights = {"#333c43", "#e67e80", "#a7c080", "#dbbc7f", "#7fbbb3", "#d699b6", "#83c092", "#d3b987"},
+
+        tab_bar = {
+            background = '#3a464c',
+
+            active_tab = {
+                bg_color = '#5c3f4f',
+                fg_color = '#d3c6aa',
+            },
+            inactive_tab = {
+                bg_color = '#48584e',
+                fg_color = '#d3c6aa',
+            },
+            inactive_tab_hover = {
+                bg_color = '#555f66',
+                fg_color = '#d3c6aa',
+                italic = false,
+            },
+            new_tab = {
+                bg_color = '#5d6b66',
+                fg_color = '#d3c6aa',
+            },
+            new_tab_hover = {
+                bg_color = '#555f66',
+                fg_color = '#d3c6aa',
+                italic = false,
+            },
+        },
+    },
+
+    ['everforest light'] = {
+        foreground = "#5c6a72",
+        background = "#f3ead3",
+        selection_bg = "#e1e4bd",
+
+        ansi = {"#f3ead3", "#f85552", "#8da101", "#dfa000", "#3a94c5", "#df69ba", "#35a77c", "#5c6a72"},
+        brights = {"#f3ead3", "#f85552", "#8da101", "#dfa000", "#3a94c5", "#df69ba", "#35a77c", "#5c6a72"},
+
+        tab_bar = {
+            background = '#ddd8be',
+
+            active_tab = {
+                bg_color = '#e1e4bd',
+                fg_color = '#5c6a72',
+            },
+            inactive_tab = {
+                bg_color = '#f4dbd0',
+                fg_color = '#5c6a72',
+            },
+            inactive_tab_hover = {
+                bg_color = '#b9c0ab',
+                fg_color = '#5c6a72',
+                italic = false,
+            },
+            new_tab = {
+                bg_color = '#eae4ca',
+                fg_color = '#5c6a72',
+            },
+            new_tab_hover = {
+                bg_color = '#b9c0ab',
+                fg_color = '#5c6a72',
+                italic = false,
+            },
+        },
+    },
+}
+
+return config
