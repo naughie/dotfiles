927ee2237ee3d610cd6d345ad25c0135f228c03e
naughie <masaton@naughie.com>
2025-08-09 18:56:03 +0900

    Add filer keymaps

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -44,6 +44,7 @@ return {
                 keymaps = {
                     global = {
                         { 'n', '<C-e>', 'new_filer' },
+                        { 'n', '<C-f>', 'move_to_filer' },
                     },
 
                     filer = {
@@ -51,6 +52,9 @@ return {
                         { 'n', '<CR>', 'open_or_expand' },
                         { 'n', 'u', 'move_to_parent' },
                         { 'n', '<C-n>', 'open_new_entry_win' },
+                        { 'n', 'd', 'open_delete_entry_win' },
+                        { 'n', 'r', 'refresh' },
+                        { 'n', 'm', 'open_rename_entry_win' },
                         { 'n', 'q', 'close_filer' },
 
                         { { 'n', 'i' }, '<C-j>', 'move_to_subwin' },
@@ -58,6 +62,13 @@ return {
 
                     new_entry = {
                         { { 'n', 'i' }, '<CR>', 'create_entry' },
+                        { 'n', 'q', 'close_subwin' },
+
+                        { { 'n', 'i' }, '<C-k>', 'move_to_filer' },
+                    },
+
+                    rename_entry = {
+                        { { 'n', 'i' }, '<CR>', 'rename_entry' },
                         { 'n', 'q', 'close_subwin' },
 
                         { { 'n', 'i' }, '<C-k>', 'move_to_filer' },
