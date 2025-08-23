a32e8f651f3b86d155e04b2148ccc69324ceb658
naughie <masaton@naughie.com>
2025-08-23 21:34:12 +0900

    Update lazy-filer config

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -46,54 +46,48 @@ return {
     {
         'naughie/lazy-filer.nvim',
         lazy = false,
-        config = function(plugin, opts)
-            opts.root_dir = plugin.dir
+        opts = function(plugin)
+            return {
+                root_dir = plugin.dir,
+                border = {
+                    hl_group = "Keyword",
+                },
+                rpc_ns = "lazy-filer",
 
-            local lf = require("lazy-filer")
-            lf.setup(opts)
+                keymaps = {
+                    global = {
+                        { 'n', '<C-e>', 'new_filer' },
+                    },
 
-            require("nvim-router").register(lf.lib_info())
+                    filer = {
+                        { 'n', 'o', 'open_or_expand' },
+                        { 'n', '<CR>', 'open_or_expand' },
+                        { 'n', 'u', 'move_to_parent' },
+                        { 'n', '<C-n>', 'open_new_entry_win' },
+                        { 'n', 'd', 'open_delete_entry_win' },
+                        { 'n', 'r', 'refresh' },
+                        { 'n', 'm', 'open_rename_entry_win' },
+                        { 'n', 'q', 'close_filer' },
+
+                        { { 'n', 'i' }, '<C-j>', 'move_to_subwin' },
+                    },
+
+                    new_entry = {
+                        { { 'n', 'i' }, '<CR>', 'create_entry' },
+                        { 'n', 'q', 'close_subwin' },
+
+                        { { 'n', 'i' }, '<C-k>', 'move_to_filer' },
+                    },
+
+                    rename_entry = {
+                        { { 'n', 'i' }, '<CR>', 'rename_entry' },
+                        { 'n', 'q', 'close_subwin' },
+
+                        { { 'n', 'i' }, '<C-k>', 'move_to_filer' },
+                    },
+                },
+            }
         end,
-        opts = {
-            border = {
-                hl_group = "Keyword",
-            },
-            rpc_ns = "lazy-filer",
-
-            keymaps = {
-                global = {
-                    { 'n', '<C-e>', 'new_filer' },
-                    { 'n', '<C-f>', 'move_to_filer' },
-                },
-
-                filer = {
-                    { 'n', 'o', 'open_or_expand' },
-                    { 'n', '<CR>', 'open_or_expand' },
-                    { 'n', 'u', 'move_to_parent' },
-                    { 'n', '<C-n>', 'open_new_entry_win' },
-                    { 'n', 'd', 'open_delete_entry_win' },
-                    { 'n', 'r', 'refresh' },
-                    { 'n', 'm', 'open_rename_entry_win' },
-                    { 'n', 'q', 'close_filer' },
-
-                    { { 'n', 'i' }, '<C-j>', 'move_to_subwin' },
-                },
-
-                new_entry = {
-                    { { 'n', 'i' }, '<CR>', 'create_entry' },
-                    { 'n', 'q', 'close_subwin' },
-
-                    { { 'n', 'i' }, '<C-k>', 'move_to_filer' },
-                },
-
-                rename_entry = {
-                    { { 'n', 'i' }, '<CR>', 'rename_entry' },
-                    { 'n', 'q', 'close_subwin' },
-
-                    { { 'n', 'i' }, '<C-k>', 'move_to_filer' },
-                },
-            },
-        },
     },
 
     {
