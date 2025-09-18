3e0f109c12f557f2f44dc1c1a413f3e016db1965
naughie <masaton@naughie.com>
2025-09-18 16:31:19 +0900

    Load lspconfig lazily

--- nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/lua/plugins/nvim-lspconfig.lua
@@ -1,6 +1,7 @@
 return {
     {
         'neovim/nvim-lspconfig',
+        event = "VeryLazy",
         config = function()
             local configs = require('lsp-configs')
             require('lsp-setup').setup(configs)
