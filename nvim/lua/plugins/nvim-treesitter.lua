24a7024df477c3a0c61a6303e281362544c8ac7d
naughie <masaton@naughie.com>
2026-01-08 01:16:40 +0900

    Disable tree-sitter's foldexpr

--- nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/lua/plugins/nvim-treesitter.lua
@@ -142,9 +142,6 @@ return {
                 group = treesitter_au,
                 callback = function()
                     vim.treesitter.start()
-                    vim.wo[0][0].foldexpr = 'v:lua.vim.treesitter.foldexpr()'
-                    vim.wo[0][0].foldmethod = 'expr'
-                    -- vim.bo.indentexpr = 'v:lua.require"nvim-treesitter".indentexpr()'
                 end,
             })
         end
